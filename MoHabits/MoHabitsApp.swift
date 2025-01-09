f762cba30a856717b87a56c85435d8cc30a55533
SeckMohameth <mohamethseck34@gmail.com>
2025-01-09 13:32:44 -0500

    Initial Commit

---
+++ MoHabits/MoHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoHabitsApp.swift
+//  MoHabits
+//
+//  Created by Mohameth Seck on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MoHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
